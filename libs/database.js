50953e4985b6ff602fc5081fe6222d3d98f5b0ef
achique-luisdan <achique.luisdan@gmail.comm>
2023-02-18 22:06:12 -0400

    refactor: :wrench: migra db a mysql v5

--- libs/database.js
+++ libs/database.js
@@ -4,7 +4,7 @@ const { configs } = require('../configs/configs');
 
 const USER = encodeURIComponent(configs.dbUser);
 const PASSWORD = encodeURIComponent(configs.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${configs.dbHost}:${configs.dbPort}/${configs.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${configs.dbHost}:${configs.dbPort}/${configs.dbName}`;
 const sequelize = new Sequelize(URI);
 
 module.exports = sequelize;
