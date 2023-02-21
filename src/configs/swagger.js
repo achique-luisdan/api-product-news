1a37144db3c4b5bb9f35b157acf6287c2328d159
achique-luisdan <achique.luisdan@gmail.comm>
2023-02-20 22:41:53 -0400

    docs: :memo: implementa swagger para documentar endpoints

---
+++ src/configs/swagger.js
@@ -0,0 +1,58 @@
+/* eslint-disable max-len */
+const path = require('path');
+
+const options = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'API REST Valuable Product News 💜',
+      description: 'Noticias o novedades de un producto digital valioso 💜',
+      version: '1.0.0',
+      servers: [
+        {
+          url: 'http://localhost:3000',
+          description: 'Development server'
+        },
+      ],
+    },
+    requestSnippets: {
+      languages: 'javascript'
+    },
+    components:{
+      securitySchemes:{
+        bearerAuth:{
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      },
+      schemas:{
+        Category : {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'Identificador de categoría',
+              example: '9a1bec'
+            },
+            name: {
+              type: 'string',
+              description: 'Nombre de categoría',
+              example: 'Eventos'
+            },
+            description: {
+              type: 'string',
+              description: 'Descripción de categoría',
+              example: 'Categoría de noticias sobre eventos o actividades.'
+            },
+          }
+        },
+      },
+    },
+  },
+
+  apis: [path.join(process.cwd(), 'src/routes/*.js')],
+
+};
+
+module.exports = options;
